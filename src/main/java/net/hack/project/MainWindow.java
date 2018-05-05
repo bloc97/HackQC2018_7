6291c0f3a3ab1071bb9010b79a92d7d96ed00348
dragonpan2 <panbeining@hotmail.com>
2018-05-04 21:43:22 -0400

    initial Graphique Push

---
+++ src/main/java/net/hack/project/MainWindow.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.hack.project;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author panbe
+ */
+public class MainWindow extends JFrame {
+
+    public MainWindow() throws HeadlessException {
+        this.setSize(1600, 900);
+        this.setVisible(true);
+    }
+    
+    
+    
+}
